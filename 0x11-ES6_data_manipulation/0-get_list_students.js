00513308162c99d439e33426fca2aa18ce257ce5
gomez5sh <emmanuel.gomez@vertebrasoluciones.com>
2021-09-25 23:26:35 -0500

    [ADD]: add arr

--- 0x11-ES6_data_manipulation/0-get_list_students.js
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,11 @@
 function getListStudents() {
-  obj = [
+  object = [
     { id: 1, firstName: "Guillaume", location: "San Francisco" },
     { id: 2, firstName: "James", location: "Columbia" },
     { id: 5, firstName: "Serena", location: "San Francisco" },
   ];
 
-  return obj;
+  return object;
 }
+
+export default getListStudents;
